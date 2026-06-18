52643d2cd20abc2f25af2acab0c47e06adbae277
sparkyLOL007 <sahilsankar05@gmail.com>
2026-06-18 16:15:41 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,7 @@ const cors=require("cors")
 const PORT=process.env.PORT || 3000
 const connectDb=require("./config/connectionDB")
 connectDb()
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://food-blog-bni7prwxk-sparky-lol-007.vercel.app"
-  ],
-  credentials: true
-}))
+app.use(cors())
 app.use(express.static("public"))
 app.use(express.json())
 app.use("/",require("./routes/user"))
